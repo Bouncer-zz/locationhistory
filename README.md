cfff18ba2ddad6b5984f8c1cbee4587969eb2550
Benjamin Timmermans <b.timmermans@vu.nl>
2017-07-08 20:05:15 +0200

    Improved filtering of GPS artifacts

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Visualize your Google location history in one map
+This tool converts your Google location history data for use in a Google Fusion Table. This allows you to see your complete location history in a zoomable and interactive Google map. You can customize the visualization to your own preference in the map settings. In order to hide artifacts in the GPS data the code splits movement when locations are too far apart. The code was tested to work with six years of location history containing 1.4 million points.
+
+![example](https://raw.githubusercontent.com/bouncer/locationhistory/master/example.png)
+
+
+# Instructions
 
 * Download your location history in JSON format via Google Takeout: https://www.google.com/settings/takeout
-* Change the location of your json file in generate.py
-* Run generate.py and your your location history will be transformed into a CSV file with connected coordinates
+* Extract the JSON file and configure its location in generate.py
+* Run generate.py using Python 2.7 and your location history will be transformed into a CSV file with connected coordinates
 * Create a new Fusion Table in Google Drive and upload your history.csv file
 * Go to the map tab to view your location history. Change the feature styles such as Line Color to an opacity of 5% to 10%
 * Change the map to terrain view to improve visibility. You can share the map via Tools -> Publish
+
--- generate.py
+++ generate.py
@@ -23,15 +23,40 @@ wr = csv.writer(w, delimiter=',')
 wr.writerow(fieldnames)
 
 output = ""
+outputCount = 0
 count = len(data['locations'])
-for c in xrange(0,count):
-    row = data['locations'][c]
 
-    if(round(row['longitudeE7'] / 10000000) != -122):
-        output += str(float(row['longitudeE7']) / 10000000) + "," + str(float(row['latitudeE7']) / 10000000) + ",0.0 ";
-	# merge every 10 000 coordinates into one line
-    if((c % 10000 == 0 and c != 0) or c == count-1):
-        wr.writerow(['<LineString><coordinates>' + output + '</LineString></coordinates>'])
-        output = ""
+for c in xrange(0,count):
+
+	# convert current location
+	lon = float(data['locations'][c]['longitudeE7']) / 10000000
+	lat = float(data['locations'][c]['latitudeE7']) / 10000000
+
+	# get next location, except if this is the last of the list
+	if c < count-1:
+		nextLon = float(data['locations'][c+1]['longitudeE7']) / 10000000
+		nextLat = float(data['locations'][c+1]['latitudeE7']) / 10000000
+	else:
+		nextLon = lon
+		nextLat = lat
+
+	output += str(lon) + "," + str(lat) + ",0.0 ";
+	outputCount += 1
+
+
+	# merge every 10 000 locations into one line or end the line if the next coordinate is too far away
+	if((c % 10000 == 0 and c != 0) or c == count-1 or abs(nextLon - lon) > 0.25 or abs(nextLat - lat) > 0.25):
+		# only save if this line has more than one coordinate
+		if outputCount > 1:
+			wr.writerow(['<LineString><coordinates>' + output + '</LineString></coordinates>'])
+			output = ""
+			outputCount = 0
+			print 'progress: ' + str(c) + '/' + str(count)
+		else:
+			output = ""
+			outputCount = 0
+			print 'skipped coordinate: ' + str(lon) + ', ' + str(lat)
+
+
 w.close()
 print 'finished'
